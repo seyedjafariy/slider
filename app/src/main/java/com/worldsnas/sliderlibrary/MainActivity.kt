eb4d028bee3a38a493989916cd67e8643a5190f5
worldsnas <worldsnas@gmail.com>
2020-03-18 22:07:52 +0100

    lowered the size of gif
    removed unnecessary widgets

--- app/src/main/java/com/worldsnas/sliderlibrary/MainActivity.kt
+++ app/src/main/java/com/worldsnas/sliderlibrary/MainActivity.kt
@@ -34,12 +34,6 @@ class MainActivity : AppCompatActivity() {
                 "https://unsplash.com/photos/tzzj8LCLujU/download?force=true"
         )
 
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
         list.layoutManager = LinearLayoutManager(this)
 
         list.withModels {
@@ -86,21 +80,5 @@ class MainActivity : AppCompatActivity() {
                 imageUrl,
                 Toast.LENGTH_SHORT
         ).show()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
